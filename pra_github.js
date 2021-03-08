89ed0de75cf044e25c414cb203064f1357267fe3
cc18262 <cc18262@kumgaku.ac.jp>
2021-03-08 23:41:45 +0900

    add A

---
+++ pra_github.js
@@ -0,0 +1,16 @@
+n = 2;
+switch (n) {
+case 1:
+  console.log("One");
+  break;
+case 2:
+  console.log("Two");
+  break;
+case 3:
+case 4:
+  console.log("Three or Four");
+  break;
+default:
+  console.log("Other");
+  break;
+}
