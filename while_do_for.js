dcc6ebbb33066800cbea3eea4dff6ee2f7666358
cc18262 <cc18262@kumgaku.ac.jp>
2021-03-09 14:36:37 +0900

    while_do_for1

---
+++ while_do_for.js
@@ -0,0 +1,23 @@
+
+var n = 0;{
+while (n < 10) {
+  console.log(n);
+  n++;
+}
+}
+
+var n = 0;{
+do {
+  console.log(n);
+  n++;
+} while (n < 10);
+}
+
+for (i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+xx = [ "Blue", "Red", "Yellow" ];
+for (i in xx) {
+  console.log(xx[i]);
+}
